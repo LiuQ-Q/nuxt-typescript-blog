de85e24a3369a2b643a963aedf3b8a1dee5c2ab1
LiuQ-Q <545784977@qq.com>
2020-06-22 23:13:02 +0800

    scrollbar style

--- layouts/default.vue
+++ layouts/default.vue
@@ -40,4 +40,31 @@ html, body {
   box-sizing: border-box;
   margin: 0;
 }
+
+html::-webkit-scrollbar {
+  /*滚动条整体样式*/
+  width : 10px;  /*高宽分别对应横竖滚动条的尺寸*/
+  height: 1px;
+}
+html::-webkit-scrollbar-thumb {
+  /*滚动条里面小方块*/
+  border-radius: 10px;
+  background-color: #01A1BB;
+  background-image: -webkit-linear-gradient(
+    45deg,
+    rgba(255, 255, 255, 0.2) 25%,
+    transparent 25%,
+    transparent 50%,
+    rgba(255, 255, 255, 0.2) 50%,
+    rgba(255, 255, 255, 0.2) 75%,
+    transparent 75%,
+    transparent
+  );
+}
+html::-webkit-scrollbar-track {
+  /*滚动条里面轨道*/
+  box-shadow   : inset 0 0 5px rgba(0, 0, 0, 0.2);
+  border-radius: 10px;
+  background   : #ededed;
+}
 </style>
